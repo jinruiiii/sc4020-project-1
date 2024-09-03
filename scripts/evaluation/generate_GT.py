e4d785df97389b0ffa1756c5961eee1553bce632
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-09-03 15:57:51 +0800

    Added function call to save ground truth

--- scripts/evaluation/generate_GT.py
+++ scripts/evaluation/generate_GT.py
@@ -2,11 +2,16 @@ import pandas as pd
 import numpy as np
 import os
 import sys
+
+
 sys.path.append("../..")
 
 from typing import List, Tuple
 from utils.load_data import load_csv
+from utils.evaultation.algo_types import AlgoType
+from utils.evaultation.dump_eval_result import dump_eval_result
 from scripts.VSMAlgo import VSM
+
 
 class Generate_GT:
     def __init__(self, dataset:str, embedding:str):
@@ -14,8 +19,8 @@ class Generate_GT:
         base_dir = os.path.dirname(__file__)
         self.dataset = load_csv(os.path.join(base_dir, f"../../data/{dataset}/raw_data/{dataset}.csv"))
         self.questions, self.index_rows = self.generate_qns(32)
-        self.answers = self.generate_top_k(10)
-        self.results_df = self.generate_df(self.index_rows, self.answers)
+        self.answers, self.time_taken = self.generate_top_k(10)
+        self.results_df = self.generate_df(self.index_rows, self.answers, self.time_taken)
 
     def generate_qns(self, sample_size:int) -> Tuple[List[str], List[int]]:
         rows = self.dataset.sample(n=sample_size)
@@ -24,20 +29,28 @@ class Generate_GT:
 
         return self.questions, self.index_rows
     
-    def generate_top_k(self, k:int) -> List[List[str]]:
+    def generate_top_k(self, k:int) -> Tuple[List[List[str]], List[float]]:
         answers = []
+        time_taken = []
         for qn in self.questions:
             answer = self.VSM.top_k_cosine_similarity(qn,k)
             answers.append(answer["Review"].values)
 
-        return answers
-    
-    def generate_df(self, index_rows, answers) -> pd.DataFrame:
-        df_dict = {}
-        df_dict['q_id'] = index_rows
-        df_dict['top_k'] = answers
+        return answers, time_taken
+
+    @staticmethod
+    def generate_df(index_rows, answers, time_taken) -> pd.DataFrame:
+        df_dict = {
+            'q_id': index_rows,
+            'top_k': answers,
+            'time_taken': time_taken
+        }
         df = pd.DataFrame.from_dict(df_dict)
         return df
-    
+
+
+
+
 if __name__ == "__main__":
     generate = Generate_GT("starbucks","bge")
+    dump_eval_result(AlgoType.VSM, generate.results_df, type="gt")
