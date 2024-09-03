4e239513f560e00e6e09d5464de4b3c8b346386e
Shreyas HEDGE <shreyas_hedge@htx.gov.sg>
2024-09-03 14:52:03 +0800

    added in df results

--- scripts/evaluation/generate_GT.py
+++ scripts/evaluation/generate_GT.py
@@ -15,6 +15,7 @@ class Generate_GT:
         self.dataset = load_csv(os.path.join(base_dir, f"../../data/{dataset}/raw_data/{dataset}.csv"))
         self.questions, self.index_rows = self.generate_qns(32)
         self.answers = self.generate_top_k(10)
+        self.results_df = self.generate_df(self.index_rows, self.answers)
 
     def generate_qns(self, sample_size:int) -> Tuple[List[str], List[int]]:
         rows = self.dataset.sample(n=sample_size)
@@ -23,14 +24,20 @@ class Generate_GT:
 
         return self.questions, self.index_rows
     
-    def generate_top_k(self, k:int):
+    def generate_top_k(self, k:int) -> List[List[str]]:
         answers = []
         for qn in self.questions:
             answer = self.VSM.top_k_cosine_similarity(qn,k)
-            answers.append(answer)
+            answers.append(answer["Review"].values)
 
         return answers
-
+    
+    def generate_df(self, index_rows, answers) -> pd.DataFrame:
+        df_dict = {}
+        df_dict['q_id'] = index_rows
+        df_dict['top_k'] = answers
+        df = pd.DataFrame.from_dict(df_dict)
+        return df
+    
 if __name__ == "__main__":
     generate = Generate_GT("starbucks","bge")
-    print(len(generate.answers), len(generate.answers[0]))
