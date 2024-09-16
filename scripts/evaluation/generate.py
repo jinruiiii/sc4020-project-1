bf0431e024e133b6d4f8d8ddbf6c4705013084dc
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-09-16 11:37:31 +0800

    Updated query to be ground truth queries

--- scripts/evaluation/generate.py
+++ scripts/evaluation/generate.py
@@ -38,16 +38,18 @@ def main():
     for n_pwr in range(3, 10):
         runners.append(lambda pwr=n_pwr: LSH("starbucks", "bge", 2**pwr))
 
+    runners.append(lambda pwr=n_pwr: LSH("starbucks", "bge", 768))
+
     print(f"Starting generator with {len(runners)} runners!")
 
     # Execute each runner
     Generator([
-        "latte",
-        "I like sour coffee",
-        "Caffeine helps me start my day",
-        "Coffeebean is better than starbucks",
-        "Water is more worth it than buying coffee from here"
-    ]).run(runners)
+        "Have been frequenting Starbucks for 30 years. Have liked most of their drinks. Their new iced apple macchiato is something I would not order again. Normally I love apple anything but disliked the flavor of this and didn’t finish… very disappointing.",
+        "Second time in a week I've gone to Starbucks and ordered pistachio grande and they didn't put the pistachio pumps in. We get it and leave with a terrible coffee for a very high price. And they expect us to tip as well! I'm not going back if I can't treat myself to what I've ordered. BTW this was on Frederica St..",
+        "I visited the Starbucks, Greensboro, NC  Sept 27, 2008, about 2:40 pm. and ordered a Grande Chai Latte with Skim. I can't find the price online, but it was something around $3.60.  I gave the cashier a $20 bill.  Being stupid, I did not count the change, or looked at the bills, except for the $10.  I  dropped the coins in the tip jar, and put the bills in my pocket.",
+        "This time this girl named KP was taking my order and when I ask her about discount she was so rude about it and she is like \"There is no button on screen that I can give you discount.\" I told her \"We always get discount\" but she was like \"There is no discount. I never saw you before.\" and she was very rude when she was talking to me.",
+        "DEMANDED TIPS FROM ME, THEN MADE ME WAIT UNTIL MY COFFEES WERE COLD AND MELTED ( I HAD 20 OF THEM). They have yet to address the problem, the store still demands tips!"
+    ], top_k=10+1).run(runners)
 
 
 if __name__ == "__main__":
