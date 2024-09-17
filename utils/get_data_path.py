1396c3ba919a1e231a26d85d2b881f13d13870e9
Tsien Jin <46650957+TsienJin@users.noreply.github.com>
2024-09-17 11:14:31 +0800

    Created helper metric utils
    - Added HNSW filepath resolver
    - Added metrics utility function for MP, MAP, nDCG

--- utils/get_data_path.py
+++ utils/get_data_path.py
@@ -1,4 +1,5 @@
 import os
+from utils.load_data import load_parquet
 
 
 def get_data_path(name: str) -> str:
@@ -15,3 +16,31 @@ def get_data_path(name: str) -> str:
         os.makedirs(path)
 
     return f"{os.path.dirname(__file__)}/../data/{name}"
+
+
+def get_hnsw_path(time_folder: str, neighbors: int, construction: int, search: int) -> str:
+    """
+    Function to get the path for HNSW parquet files
+    :param time_folder: e.g. "2024-09-16_06-03-58"
+    :param neighbors: number of neighbours used in the runner
+    :param construction: corresponding to efConstruction
+    :param search: corresponding to efSearch
+    :return: absolute file path to the datafile
+    """
+    file = f"starbucks_AlgoType.HNSW__embedding=bge_mode=hnsw_neighbors={neighbors}_efConstruction={construction}_efSearch={search}.parquet"
+    path = get_data_path(f"eval/{time_folder}/{file}")
+    assert os.path.exists(path), f"File does not exist! {path}"
+    return path
+
+
+def get_lsh_path(time_folder: str, nbits: int) -> str:
+    """
+    Function
+    :param time_folder: e.g. "2024-09-16_06-03-58"
+    :param nbits: number of bits used in LSH runner
+    :return: absolute file path to the datafile
+    """
+    file = f"starbucks_AlgoType.LSH__embedding=bge_mode=hnsw_similarity_nbits={nbits}.parquet"
+    path = get_data_path(f"eval/{time_folder}/{file}")
+    assert os.path.exists(path), f"File does not exist! {path}"
+    return path
